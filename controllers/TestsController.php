aae87b7ff690818910e043baf606a22a6dd75b77
cja <chenjiaao@zzwealth.cn>
2019-03-29 17:34:35 +0800

    init

--- controllers/TestsController.php
+++ controllers/TestsController.php
@@ -5,12 +5,11 @@ class TestsController extends BaseController
 {
     public function test()
     {
-        dd(microtime(true) - AOPHP_START);
 
 //        echo env('APP_NAME', 'mysql');
 //        die;
-//        $this->smarty->assign(['name' => '张三三']);
-//
-//        $this->smarty->display('test.html');
+        $this->smarty->assign(['name' => '张三三']);
+
+        $this->smarty->display('test.html');
     }
 }
